488e2927c9c158c900f32fd56f97d99947ce913d
Bruno Breyer Garcia <bruno.garcia99@edu.pucrs.br>
2023-05-31 22:31:03 -0300

    falta arrumar as portas

--- Go/receiver/receiver.go
+++ Go/receiver/receiver.go
@@ -1,5 +1,9 @@
 package main
 
 func main() {
-	// Read the contents of the .txt file
+	UDP_IP := "127.0.0.1"
+	UDP_PORT := 5001
+
+	// Create a UDP socket
+	udpSocket := createUDPSocket(UDP_IP, UDP_PORT)
 }
--- Go/sender/sender.go
+++ Go/sender/sender.go
@@ -7,21 +7,32 @@ import (
 )
 
 func main() {
-	UDP_IP := "127.0.0.1"
-	UDP_PORT := 34754
-	CHUNK_SIZE := 1024
+	stop := make(chan bool)
+	packetReceived := make(chan int)
 
-	// Transfer the file to bytes
-	payload := fileToBytes("send.txt")
+	UDP_IP := "127.0.0.1"
+	UDP_PORT := 5000
+	// UDP_PORT_TO_SEND = 5001
+	CHUNK_SIZE := 16
 
 	// Create a UDP socket
 	udpSocket := createUDPSocket(UDP_IP, UDP_PORT)
 
+	// Listen for incoming acks
+	go ackListener(packetReceived, udpSocket)
+
+	// Transfer the file to bytes
+	bytes := fileToBytes("send.txt")
+
+	// Create the payload from the bytes
+	payload := payloadSeparation(bytes, CHUNK_SIZE)
+
 	// Send the payload
-	send(udpSocket, payload, CHUNK_SIZE)
+	go sender(udpSocket, payload, CHUNK_SIZE, stop, packetReceived)
 
 	// Close the socket
 	udpSocket.Close()
+	<-stop
 }
 
 func fileToBytes(filePath string) []byte {
@@ -37,32 +48,99 @@ func fileToBytes(filePath string) []byte {
 
 	// Use the bytes as needed
 	fmt.Printf("Bytes: %v\n", bytes)
+	fmt.Printf("Bytes as string: %d\n", len(bytes))
 
 	return bytes
 }
 
 func createUDPSocket(UDP_IP string, UDP_PORT int) *net.UDPConn {
-	// Create the address
-	UDP_ADDR := net.UDPAddr{
-		Port: UDP_PORT,
-		IP:   net.ParseIP(UDP_IP),
+	// Resolve the UDP address
+	udpAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", UDP_IP, UDP_PORT))
+	if err != nil {
+		fmt.Printf("Error resolving UDP address: %s\n", err)
+		return nil
 	}
 
-	// Create the UDP socket
-	udpSocket, err := net.DialUDP("udp", nil, &UDP_ADDR)
+	// Create a UDP socket
+	udpConn, err := net.ListenUDP("udp", udpAddr)
 	if err != nil {
 		fmt.Printf("Error creating UDP socket: %s\n", err)
 		return nil
 	}
 
-	return udpSocket
+	return udpConn
 }
 
-func send(udpSocket *net.UDPConn, payload []byte, CHUNK_SIZE int) {
-	_, err := udpSocket.Write(payload)
+func sender(udpSocket *net.UDPConn, payload [][]byte, CHUNK_SIZE int, stop chan bool, packetReceived chan int) {
+	currentPacket := 0
+	for {
+		if currentPacket == len(payload) {
+			break
+		}
+
+		fmt.Printf("\nSending payload...\n")
+		fmt.Printf("Payload: %v\n", payload[currentPacket])
+		send(udpSocket, payload[currentPacket], packetReceived)
+
+		// Wait for ack
+		fmt.Printf("\nWaiting for ack...\n")
+		<-packetReceived
+
+		currentPacket = currentPacket + 1
+	}
+	stop <- true
+}
+
+func send(udpSocket *net.UDPConn, packet []byte, packetReceived chan int) {
+	_, err := udpSocket.Write(packet)
 	if err != nil {
 		fmt.Printf("Error sending payload: %s\n", err)
 		return
 	}
-	fmt.Printf("Payload sent successfully\n")
+	fmt.Printf("\nPayload sent successfully\n")
+}
+
+func payloadSeparation(payload []byte, partSize int) [][]byte {
+	array := make([]byte, 0)
+	array = append(array, payload...)
+
+	if len(array)%partSize != 0 {
+		filler := partSize - (len(array) % partSize)
+
+		for i := 0; i < filler; i++ {
+			array = append(array, byte(0))
+		}
+	}
+
+	quantPackets := len(array) / partSize
+
+	arrayIndex := 0
+	matrix := make([][]byte, quantPackets)
+	for i := 0; i < quantPackets; i++ {
+		matrix[i] = make([]byte, partSize)
+		for j := 0; j < partSize; j++ {
+			matrix[i][j] = array[arrayIndex]
+			arrayIndex++
+		}
+	}
+
+	fmt.Printf("Payload: %v\n", matrix)
+
+	return matrix
+}
+
+func ackListener(packetReceived chan int, udpSocket *net.UDPConn) {
+	for {
+		// // Listen for incoming acks
+		// udpAddr, err := udpSocket.Read(payload)
+		// if err != nil {
+		// 	fmt.Printf("Error resolving UDP address: %s\n", err)
+		// 	return
+		// }
+
+		// udpConn, err := net.ListenUDP("udp", udpAddr)
+
+		fmt.Printf("\nPacket Ack: Sucess!")
+		packetReceived <- 1
+	}
 }
